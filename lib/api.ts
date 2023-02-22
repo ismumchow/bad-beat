1d94a81219c1effcbb1ca19acae3590fa4c9b8a4
ismum <ichowdhury4@fordham.edu>
2023-02-22 10:48:47 -0500

    added button glassplane card component and sidebar component

---
+++ lib/api.ts
@@ -0,0 +1,17 @@
+export const fetcher = async ({ url, method, body, json = true }) => {
+    const res = await fetch(url, {
+        method,
+        ...(body && {body: JSON.stringify(body)}),
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }) 
+
+    if (!res.ok) throw new Error("API ERROR");
+    
+    if(json){
+        const data = await res.json()
+        return data.data
+    }
+}
